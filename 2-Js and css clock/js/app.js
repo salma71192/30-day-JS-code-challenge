30a2365a93c5a0dbb0483049d4566233a26c2126
Salma Mohammed <sm.71175@gmail.com>
2018-03-15 01:08:53 +0200

    Fix: sync the current seconds with the transition.

--- 2-Js and css clock/js/app.js
+++ 2-Js and css clock/js/app.js
@@ -2,7 +2,7 @@ const secondHand = document.querySelector('.second-hand');
 function setDate() {
   const now = new Date();
   const sec = now.getSeconds();
-  const secDegrees = ((sec / 60) * 360);
+  const secDegrees = ((sec / 60) * 360) + 90;
   console.log(sec);
 
   secondHand.style.transform = `rotate(${secDegrees}deg)`;
