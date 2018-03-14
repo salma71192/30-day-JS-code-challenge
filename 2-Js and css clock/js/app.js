94bdd425c278416405142e9dfac5d08dc15eeeeb
Salma Mohammed <sm.71175@gmail.com>
2018-03-15 01:21:41 +0200

    Feat: Add minutes-hand functionality.

--- 2-Js and css clock/js/app.js
+++ 2-Js and css clock/js/app.js
@@ -1,11 +1,24 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
 function setDate() {
   const now = new Date();
+  /*
+   * Add Seconds Functionality
+   */
+  // get the current seconds
   const sec = now.getSeconds();
   const secDegrees = ((sec / 60) * 360) + 90;
-  console.log(sec);
-
+  // Move the seconds hand
   secondHand.style.transform = `rotate(${secDegrees}deg)`;
+
+  /*
+   * Add Minutes Functionality
+   */
+  // get the current minutes
+  const Min = now.getMinutes();
+  const MinDegrees = ((Min / 60) * 360) + 90;
+  // Move the seconds hand
+  minuteHand.style.transform = `rotate(${MinDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
