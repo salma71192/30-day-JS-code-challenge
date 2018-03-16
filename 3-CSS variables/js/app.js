3fb8e905d055bc29c59cc730e04a629c89ec5057
Salma Mohammed <sm.71175@gmail.com>
2018-03-17 00:03:37 +0200

    Fix: a typo.

--- 3-CSS variables/js/app.js
+++ 3-CSS variables/js/app.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleChange() {
   const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperity(`--${this.name}`, this.value);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleChange));
